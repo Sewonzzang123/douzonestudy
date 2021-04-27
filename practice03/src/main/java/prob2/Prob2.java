9bb96f2578ddcc7a6d486a1bb535bdace0dee6c4
BIT <BIT@DESKTOP-01G2BDF>
2021-04-27 11:38:53 +0900

    practice문제 업로드

--- practice03/src/main/java/prob2/Prob2.java
+++ practice03/src/main/java/prob2/Prob2.java
@@ -1,7 +1,12 @@
 package prob2;
 
+import java.util.Scanner;
+
 public class Prob2 {
 	public static void main(String[] args) {
-		/* 코드 작성 */
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("숫자를 입력하세요: ");
+		int number = scanner.nextInt();
 	}
 }
