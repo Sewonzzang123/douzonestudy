0ffc5092e5e4a2a2d49dc7717c6dc8f678f67f1c
BIT <BIT@DESKTOP-01G2BDF>
2021-04-30 17:45:10 +0900

    구구단 하는중

---
+++ finalTest/src/main/java/prob01/GugudanTest.java
@@ -0,0 +1,47 @@
+package prob01;
+
+import java.util.Scanner;
+
+public class GugudanTest {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		Gugudan[] gugus = makeNineGugu();
+		Gugudan quiz = gugus[(int)(Math.random()*9)];
+		System.out.println(quiz.getLeft()+" x "+quiz.getRight()+" = ?");
+		
+		
+		for(int i=1; i<4; i++) {
+			for(int j=1; j<4; j++) {
+				System.out.print(gugus[(i*j)-1].multi()+" ");			
+			}
+			System.out.println();
+		}
+		
+		System.out.print("answer: ");
+		int answer = scanner.nextInt();
+		if(quiz.result() == answer) {
+			System.out.println("정답");
+		}else {
+			System.out.println("오답");
+		}
+		
+	}
+	
+	public static Gugudan[] makeNineGugu() {		
+		Gugudan[] gugus = new Gugudan[8];
+		boolean max = false;
+		
+		while(!max) {
+			Gugudan gugu = new Gugudan
+					((int)(Math.random()*9)+1,(int)(Math.random()*9)+1);
+			//여기부터 채워야 해
+			
+			
+		}
+		
+		return gugus;
+	}
+
+}
