393602d4887665a1d6ed1be47b49dc5373cbc9f9
BIT <BIT@DESKTOP-01G2BDF>
2021-05-03 10:58:31 +0900

    io #2

---
+++ chapter04/src/main/java/io/FileReaderTest.java
@@ -0,0 +1,60 @@
+package io;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+
+public class FileReaderTest {
+
+	public static void main(String[] args) {
+		Reader in = null;
+		InputStream is = null;
+		
+		try {
+			in = new FileReader("1234.txt");
+			
+			int data = -1;
+			int count = 0;
+			
+			while((data = in.read())!= -1) {
+				System.out.print((char)data);
+				count++;
+			}
+			
+			System.out.println();
+			System.out.println("count: "+count);
+			
+			System.out.println("====================");
+			
+			count = 0;
+			data = -1;
+			is = new FileInputStream("1234.txt");
+			while((data = is.read())!= -1) {
+				//문자는 깨져 한바이트씩 읽었기 때문에
+				System.out.print((char)data);
+				count++;
+			}
+			
+			System.out.println();
+			System.out.println("count: "+count);
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("file not found: "+e);
+		}  catch (IOException e) {
+			System.out.println("error: "+e);
+		} finally {
+			try {
+				if(in != null) {
+					in.close();
+				}				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
