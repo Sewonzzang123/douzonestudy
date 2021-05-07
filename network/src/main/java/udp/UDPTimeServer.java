d35abe3ef4f47c972bfa19ea763e36327c181d34
BIT <BIT@DESKTOP-01G2BDF>
2021-05-07 14:44:42 +0900

    time

--- network/src/main/java/udp/UDPTimeServer.java
+++ network/src/main/java/udp/UDPTimeServer.java
@@ -1,20 +1,53 @@
 package udp;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class UDPTimeServer {
+	public static final int PORT = 7777;
+	public static final int BUFFER_SIZE = 1024;
 
 	public static void main(String[] args) {
-		
-		//데이터 수신
-		DatagramPacket receivePakcet = new DatagramPacket(null, 0);
-		//socket.receive(receivePakcet); //blocking
-		//데이터 송신
-		new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date());
-		
+		DatagramSocket socket = null;
+		try {
+			// 데이터 수신
+			socket = new DatagramSocket(PORT);
+			while (true) {
 
+				DatagramPacket receivePacket = new DatagramPacket(new byte[BUFFER_SIZE], BUFFER_SIZE);
+				socket.receive(receivePacket);// blocking
+
+				byte[] receiveData = receivePacket.getData();
+				int length = receivePacket.getLength();
+				String message = new String(receiveData, 0, length, "utf-8");
+
+				System.out.println("[server] receive: " + message);
+				byte[] sendData;
+				if ("".equals(message)) {
+					// 3.시간 데이터 송신
+					String data = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date());
+					sendData = data.getBytes();
+				} else {
+					// 3.아니면 그냥 데이터 송신
+					sendData = message.getBytes("utf-8");
+				}
+				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(),
+						receivePacket.getPort());
+				socket.send(sendPacket);
+			}
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (socket != null && socket.isClosed() == false) {
+				socket.close();
+			}
+		}
 	}
 
 }
